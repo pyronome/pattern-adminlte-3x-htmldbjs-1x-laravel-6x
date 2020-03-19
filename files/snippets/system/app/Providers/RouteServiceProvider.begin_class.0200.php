7febfc07ce71abda3a6be7d3d802853a5afb029c
Aykut Aydınlı <aykut@pyronome.com>
2020-03-19 13:41:22 +0300

    Files updated

---
+++ files/snippets/system/app/Providers/RouteServiceProvider.begin_class.0200.php
@@ -0,0 +1,1 @@
+use App\Http\Middleware\AdminMiddleware;
---
+++ files/snippets/system/app/Providers/RouteServiceProvider.end_methods.0010.php
@@ -0,0 +1,14 @@
+    /**
+     * Define the "admin" routes for the application.
+     *
+     * These routes are typically stateless.
+     *
+     * @return void
+     */
+    protected function mapAdminRoutes()
+    {
+        Route::prefix('admin')
+             ->middleware(AdminMiddleware::class)
+             ->namespace('App\Http\Controllers\Admin')
+             ->group(base_path('routes/admin.php'));
+    }
