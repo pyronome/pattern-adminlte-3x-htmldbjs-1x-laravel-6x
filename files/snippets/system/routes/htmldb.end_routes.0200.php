c53ad1b3a49075286d6c2718c3c7a8ff49d694bb
Aykut Aydınlı <aykut@pyronome.com>
2020-03-21 21:23:52 +0300

    Snippets updated

--- files/snippets/system/routes/htmldb.end_routes.0200.php
+++ files/snippets/system/routes/htmldb.end_routes.0200.php
@@ -1,4 +1,11 @@
-Route::prefix('adminlte')->group(function () {
+$adminLTEFolder = getenv('ADMINLTE_FOLDER');
+
+if ($adminLTEFolder === false)
+{
+    $adminLTEFolder = 'adminlte';
+} // if ($adminLTEFolder === false) {
+
+Route::prefix($adminLTEFolder)->group(function () {
     Route::prefix('login')->group(function () {
 	    Route::get('/get', 'AdminLTE/LoginController@getHTMLDB');
     });
