6c8008ab921b6085248a0931e2f4d0e3d91b1820
Aykut Aydınlı <aykut@pyronome.com>
2020-04-06 14:29:59 +0300

    AdminLTEUserGroupController updated

--- files/source/app/Http/Controllers/AdminLTE/AdminLTEUserGroupController.php
+++ files/source/app/Http/Controllers/AdminLTE/AdminLTEUserGroupController.php
@@ -29,4 +29,54 @@ class AdminLTEUserGroupController extends Controller
         return view($viewName, $viewData);
     }
 
+    public function showDetailPage(Request $request)
+    {
+
+        $viewName = ('adminlte.'
+                . $this->controllerName
+                . '_detail');
+
+        if (view()->exists('adminlte.custom.'
+                . $this->controllerName
+                . '_detail'))
+        {
+            $viewName = 'adminlte.custom.'
+                    . $this->controllerName
+                    . '_detail';
+        } // if (view()->exists('adminlte.custom.'
+
+        $adminLTE = new AdminLTE();
+
+        $viewData['controllerName'] = $this->controllerName;
+        $viewData['user'] = $adminLTE->getUserData();
+
+        return view($viewName, $viewData);
+
+    }
+
+    public function showEditPage(Request $request)
+    {
+
+        $viewName = ('adminlte.'
+                . $this->controllerName
+                . '_edit');
+
+        if (view()->exists('adminlte.custom.'
+                . $this->controllerName
+                . '_edit'))
+        {
+            $viewName = 'adminlte.custom.'
+                    . $this->controllerName
+                    . '_edit';
+        } // if (view()->exists('adminlte.custom.'
+
+        $adminLTE = new AdminLTE();
+
+        $viewData['controllerName'] = $this->controllerName;
+        $viewData['user'] = $adminLTE->getUserData();
+
+        return view($viewName, $viewData);
+
+    }
+
 }
