d07b1db4e3c5d9d388fc324fac0d0e8d2c9728e0
Aykut Aydınlı <aykut@pyronome.com>
2020-04-06 16:41:34 +0300

    header blade view updated

--- files/source/resources/views/adminlte/header.blade.php
+++ files/source/resources/views/adminlte/header.blade.php
@@ -19,7 +19,6 @@
                 </li>
             </ul>
         </nav>
-		
         <!-- Main Sidebar Container -->
         <aside class="main-sidebar sidebar-dark-primary elevation-4">
             <!-- Brand Logo -->
