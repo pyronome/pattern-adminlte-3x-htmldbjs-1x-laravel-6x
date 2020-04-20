6be082f113e683b67b4a668e776bdc717a7e7d05
hakanerdal <hakan.erdal@sanpark.com>
2020-04-20 15:26:23 +0300

    Update 2020_03_23_000000_create_adminltelayout_table.php
    setAdminLTEDefaultLayout function call added

--- files/source/database/migrations/2020_03_23_000000_create_adminltelayout_table.php
+++ files/source/database/migrations/2020_03_23_000000_create_adminltelayout_table.php
@@ -3,6 +3,7 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+use App\AdminLTE;
 
 /* {{snippet:begin_class}} */
 
@@ -32,6 +33,9 @@ class CreateAdminLTELayoutTable extends Migration
             $table->string('pagename');
             $table->text('widgets');
         });
+
+        $adminLTE = new AdminLTE();
+        $adminLTE->setAdminLTEDefaultLayout();
 
         /* {{snippet:end_up_method}} */
     }
