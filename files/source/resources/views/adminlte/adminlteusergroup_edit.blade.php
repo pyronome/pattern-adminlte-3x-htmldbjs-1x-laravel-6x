6b53c5b0a86414a3283149f7d35694e0e4ca1b7e
hakanerdal <hakan.erdal@sanpark.com>
2020-05-16 17:10:58 +0300

    Update adminlteusergroup_edit.blade.php

--- files/source/resources/views/adminlte/adminlteusergroup_edit.blade.php
+++ files/source/resources/views/adminlte/adminlteusergroup_edit.blade.php
@@ -37,18 +37,6 @@
                                         data-htmldb-field="id"
                                         value=""
                                         data-htmldb-reset-value="0">
-                                    <input type="hidden"
-                                        id="formUserGroup-menu_permission"
-                                        name="formUserGroup-menu_permission"
-                                        class="htmldb-field"
-                                        data-htmldb-field="menu_permission"
-                                        value=""/>
-                                    <input type="hidden"
-                                        id="formUserGroup-service_permission"
-                                        name="formUserGroup-service_permission"
-                                        class="htmldb-field"
-                                        data-htmldb-field="service_permission"
-                                        value=""/>
                                     <div class="row">
                                         <div class="form-group col-lg-12 col-md-12 col-xs-12">
                                             <div class="icheck-primary d-inline">
@@ -92,99 +80,6 @@
                                                 data-htmldb-value="@{{title}}">
                                         </div>
                                     </div>
-                                    <div class="row">
-                                        <div class="col-lg-12 col-md-12 col-xs-12">
-                                            <label>{{ __('Menu Permissions') }}</label>
-                                            <div class="divPermissionContainer">
-                                                <table class="table table-bordered table-permission">
-                                                    <thead>
-                                                        <tr>
-                                                            <th></th>
-                                                            <th class="text-left">
-                                                                <div class="icheck-primary d-inline">
-                                                                    <input type="checkbox"
-                                                                        id="input-view-all"
-                                                                        name="input-view-all"
-                                                                        value="1">
-                                                                    <label for="input-view-all" class="detail-label">
-                                                                        {{ __('View Permission') }}
-                                                                    </label>
-                                                                </div>
-                                                            </th>
-                                                            <th class="text-left">
-                                                                <div class="icheck-primary d-inline">
-                                                                    <input type="checkbox"
-                                                                        id="input-edit-all"
-                                                                        name="input-edit-all"
-                                                                        value="1">
-                                                                    <label for="input-edit-all" class="detail-label">
-                                                                        {{ __('Edit Permission') }}
-                                                                    </label>
-                                                                </div>
-                                                            </th>
-                                                        </tr>
-                                                    </thead>
-                                                    <tbody id="tbodyMenuPermission">
-                                                        @include('adminlte.adminlteusergroup_permissions_edit')
-                                                    </tbody>
-                                                </table>        
-                                            </div>
-                                        </div>
-                                    </div>
-                                    <div class="row">
-                                        <div class="col-lg-12 col-md-12 col-xs-12">
-                                            <label>{{ __('Service Permissions') }}</label>
-                                            <div class="divPermissionContainer">
-                                                <table class="table table-bordered table-permission">
-                                                    <thead>
-                                                        <tr>
-                                                            <th>
-                                                                <button type="button"
-                                                                    id="buttonEditServices"
-                                                                    class="btn btn-primary btn-xs btn-on-table">
-                                                                    <i class="fa fa-plus"></i> <span class="hidden-xxs">{{ __('Add') }}</span>
-                                                                </button>
-                                                            </th>
-                                                            <th class="text-left">
-                                                                <div class="icheck-primary d-inline">
-                                                                    <input type="checkbox"
-                                                                        id="input-get-all"
-                                                                        name="input-get-all"
-                                                                        value="1">
-                                                                    <label for="input-get-all" class="detail-label">
-                                                                        {{ __('Get Permission') }}
-                                                                    </label>
-                                                                </div>
-                                                            </th>
-                                                            <th class="text-left">
-                                                                <div class="icheck-primary d-inline">
-                                                                    <input type="checkbox"
-                                                                        id="input-post-all"
-                                                                        name="input-post-all"
-                                                                        value="1">
-                                                                    <label for="input-post-all" class="detail-label">
-                                                                        {{ __('Post Permission') }}
-                                                                    </label>
-                                                                </div>
-                                                            </th>
-                                                            <th class="text-left">
-                                                                <div class="icheck-primary d-inline">
-                                                                    <input type="checkbox"
-                                                                        id="input-delete-all"
-                                                                        name="input-delete-all"
-                                                                        value="1">
-                                                                    <label for="input-delete-all" class="detail-label">
-                                                                        {{ __('Delete Permission') }}
-                                                                    </label>
-                                                                </div>
-                                                            </th>
-                                                        </tr>
-                                                    </thead>
-                                                    <tbody id="tbodyServicePermission"></tbody>
-                                                </table>
-                                            </div>
-                                        </div>
-                                    </div>
                                 </form>
 
                                 <div class="card-footer show_by_permission">
@@ -204,160 +99,16 @@
                 </div>
             </section>
         </div>
-        
     </div>
-
-    <div class="modal fade" id="modal-ServiceList" tabindex="-1" role="dialog">
-        <div class="modal-dialog modal-lg">
-            <div class="modal-content">
-                <div class="modal-header">
-                    <h4 class="modal-title">{{ __('Services') }}</h4>
-                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                        <span aria-hidden="true">&times;</span>
-                    </button>
-                </div>
-                <div class="modal-body" style="padding-top:0px;">
-                    <div class="row">
-                        <div class="col-md-12 col-sm-12 col-xs-12 mb-10">
-                            <div class="input-group input-group-md" style="padding: 5px;margin-top: 15px;">
-                                <input type="search"
-                                    id="searchService" name="searchService"
-                                    class="form-control float-right inputSearchBar"
-                                    placeholder="{{ __('Search') }}">
-                                <div class="input-group-append labelSearchBar">
-                                    <button type="button" class="btn btn-default">
-                                        <i class="fas fa-search text-primary"></i>
-                                    </button>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="row">
-                        <div class="col-md-12">
-                            <ul id="ulDirectory" class="list-group ul-permission-directories">
-
-                            </ul>
-                        </div>
-                    </div>
-                </div>
-                <div class="modalfooter justify-content-between show_by_permission">
-                    <div class="row">
-                        <div class="col-md-12">
-                            <button type="button" class="btn btn-outline-secondary float-left" data-dismiss="modal">{{ __('Cancel') }}</button>
-                            <button type="button"
-                                id="buttonSave-Services"
-                                name="buttonSave-Services"
-                                class="btn btn-success float-right">
-                                {{ __('Save') }}
-                            </button>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
+    
     @include('adminlte.footer')
-    <div id="directoryHTMLDB"
-        class="htmldb-table"
-        data-htmldb-priority="0"
-        data-htmldb-read-url="/{{ config('adminlte.main_folder') }}/htmldb/__services/get_directories?_token={{ csrf_token() }}"
-        data-htmldb-read-only="1">
-    </div>
-    @verbatim
-    <script type="text/html" 
-        id="ulDirectoryTemplate"
-        class="htmldb-template"
-        data-htmldb-table="directoryHTMLDB"
-        data-htmldb-template-target="ulDirectory">
-        <li class="searchable-service li-directory" data-search-text="{{title}}" data-type="directory">
-            <div class="icheck-primary d-inline">
-                <input type="checkbox"
-                    id="select{{title}}Services"
-                    name="select{{title}}Services"
-                    class="selectDirectoryServices"
-                    data-directory="{{title}}">
-                <label for="select{{title}}Services" class="detail-label">
-                    {{title}}
-                </label>
-            </div>
-            <ul id="{{title}}Services" class="ul-permission-services"></ul>
-        </li>
-    </script>
-    @endverbatim
-    <div id="fileHTMLDB"
-        class="htmldb-table"
-        data-htmldb-priority="1"
-        data-htmldb-read-url="/{{ config('adminlte.main_folder') }}/htmldb/__services/get?_token={{ csrf_token() }}"
-        data-htmldb-read-only="1">
-    </div>
-    @verbatim
-    <script type="text/html" 
-        id="ulServiceTemplate"
-        class="htmldb-template"
-        data-htmldb-table="fileHTMLDB"
-        data-htmldb-template-target="{{directory_name}}Services">
-        <li class="searchable-service li-service" data-search-text="{{title}}" data-type="service">
-            <div class="icheck-primary d-inline">
-                <input type="checkbox"
-                    id="selectService{{title}}"
-                    name="selectService{{title}}"
-                    class="{{directory_name}}Service selectService"
-                    data-directory="{{directory_name}}"
-                    data-file="{{file_name}}">
-                <label for="selectService{{title}}" class="detail-label">
-                    {{title}}
-                </label>
-            </div>
-        </li>
-    </script>
-    <script type="text/html" id="trServicePermissionInputs">
-        <tr>
-            <td scope="row">__DIRECTORY__/__SERVICE__</td>
-            <td class="text-left">
-                <div class="icheck-primary d-inline">
-                    <input type="checkbox"
-                        id="input-__DIRECTORY__/__SERVICE__/g"
-                        name="input-__DIRECTORY__/__SERVICE__/g"
-                        class="input-service-permission input-get"
-                        data-permission-token="__DIRECTORY__/__SERVICE__/g"
-                        value="1">
-                    <label for="input-__DIRECTORY__/__SERVICE__/g" class="detail-label">
-                    </label>
-                </div>
-            </td>
-            <td class="text-left">
-                <div class="icheck-primary d-inline">
-                    <input type="checkbox"
-                        id="input-__DIRECTORY__/__SERVICE__/p"
-                        name="input-__DIRECTORY__/__SERVICE__/p"
-                        class="input-service-permission input-post"
-                        data-permission-token="__DIRECTORY__/__SERVICE__/p"
-                        value="1">
-                    <label for="input-__DIRECTORY__/__SERVICE__/p" class="detail-label">
-                    </label>
-                </div>
-            </td>
-            <td class="text-left">
-                <div class="icheck-primary d-inline">
-                    <input type="checkbox"
-                        id="input-__DIRECTORY__/__SERVICE__/d"
-                        name="input-__DIRECTORY__/__SERVICE__/d"
-                        class="input-service-permission input-delete"
-                        data-permission-token="__DIRECTORY__/__SERVICE__/d"
-                        value="1">
-                    <label for="input-__DIRECTORY__/__SERVICE__/d" class="detail-label">
-                    </label>
-                </div>
-            </td>
-        </tr>
-    </script>
-    @endverbatim
+    
     <div id="AdminLTEUserGroupHTMLDB"
         class="htmldb-table"
         data-htmldb-priority="2"
         data-htmldb-read-url="/{{ config('adminlte.main_folder') }}/htmldb/adminlteusergroup/get/@{{$URL.-1}}?_token={{ csrf_token() }}"
         data-htmldb-write-url="/{{ config('adminlte.main_folder') }}/htmldb/adminlteusergroup/post?_token={{ csrf_token() }}"
-        data-htmldb-redirect="adminlteusergroup/last"
+        data-htmldb-redirect="/{{ config('adminlte.main_folder') }}/adminlteusergroup/last"
         data-htmldb-loader="divLoader">
     </div>
 
@@ -380,6 +131,6 @@
     <script src="/assets/adminlte/js/global.js"></script>
     <script src="/assets/adminlte/js/htmldb.js"></script>
     <script src="/assets/adminlte/js/adminlte.htmldb.js"></script>
-    <script src="/assets/adminlte/js/adminlte_user_group_edit.js"></script>
+    <script src="/assets/adminlte/js/adminlteusergroup_edit.js"></script>
 </body>
 </html>
