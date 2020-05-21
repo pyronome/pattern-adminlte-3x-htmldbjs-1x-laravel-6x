ef93372d07c11d8b456545e7708cff7c1a879bb9
hakanerdal <hakan.erdal@sanpark.com>
2020-05-21 18:49:16 +0300

    Update root_settings.blade.php

--- files/source/resources/views/adminlte/root_settings.blade.php
+++ files/source/resources/views/adminlte/root_settings.blade.php
@@ -1,5 +1,5 @@
 @include('adminlte.head')
-<body class="hold-transition sidebar-mini layout-fixed" data-url-prefix="" data-page-url="profile">
+<body class="sidebar-mini layout-fixed control-sidebar-slide-open {{ $customization['body'] }}" data-url-prefix="" data-page-url="profile">
     @include('adminlte.header')
         <div class="content-wrapper">
             <div class="content-header">
