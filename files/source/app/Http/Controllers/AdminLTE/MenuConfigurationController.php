bd3eeb60df4b80f158a31f0cfa6c07d24f2633de
hakanerdal <hakan.erdal@sanpark.com>
2020-05-21 18:54:25 +0300

    Update MenuConfigurationController.php

--- files/source/app/Http/Controllers/AdminLTE/MenuConfigurationController.php
+++ files/source/app/Http/Controllers/AdminLTE/MenuConfigurationController.php
@@ -25,7 +25,8 @@ class MenuConfigurationController extends Controller
 
         $viewData['controllerName'] = $this->controllerName;
         $viewData['user'] = $adminLTE->getUserData();
-
+        $viewData['customization'] = $adminLTE->getCustomization();
+        
         return view($viewName, $viewData);
     }
 
