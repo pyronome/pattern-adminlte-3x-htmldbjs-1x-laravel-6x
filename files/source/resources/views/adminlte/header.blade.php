2a9697519a6c3f9a59db4b570c5e10f8388a8a40
hakanerdal <hakan.erdal@sanpark.com>
2020-05-21 18:46:32 +0300

    Update header.blade.php

--- files/source/resources/views/adminlte/header.blade.php
+++ files/source/resources/views/adminlte/header.blade.php
@@ -1,7 +1,7 @@
 
 	<div class="wrapper">
     	<!-- Navbar -->
-        <nav class="main-header navbar navbar-expand navbar-dark">
+        <nav class="main-header navbar navbar-expand {{ $customization['main-header'] }}">
             <!-- Left navbar links -->
             <ul class="navbar-nav">
                 <li class="nav-item">
@@ -20,9 +20,9 @@
             </ul>
         </nav>
         <!-- Main Sidebar Container -->
-        <aside class="main-sidebar sidebar-dark-primary elevation-4">
+        <aside class="main-sidebar {{ $customization['main-sidebar'] }} elevation-4">
             <!-- Brand Logo -->
-            <a href="/{{ config('adminlte.main_folder') }}/profile/detail" class="brand-link">
+            <a href="/{{ config('adminlte.main_folder') }}/profile/detail" class="brand-link {{ $customization['brand-link'] }}">
                 <img src="{{ $user['image'] }}" alt="User Image" class="brand-image img-circle elevation-3" style="opacity: .8">
                 <span class="brand-text font-weight-light">{{ $user['name'] }}</span>
             </a>
