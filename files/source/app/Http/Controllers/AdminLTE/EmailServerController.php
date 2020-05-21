7141d440c6ea3c96a6d00be1b090070c397bf5fe
hakanerdal <hakan.erdal@sanpark.com>
2020-05-21 18:53:46 +0300

    Update EmailServerController.php

--- files/source/app/Http/Controllers/AdminLTE/EmailServerController.php
+++ files/source/app/Http/Controllers/AdminLTE/EmailServerController.php
@@ -25,7 +25,8 @@ class EmailServerController extends Controller
 
         $viewData['controllerName'] = $this->controllerName;
         $viewData['user'] = $adminLTE->getUserData();
-
+        $viewData['customization'] = $adminLTE->getCustomization();
+        
         return view($viewName, $viewData);
     }
 
